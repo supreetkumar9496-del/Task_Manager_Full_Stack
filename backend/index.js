6baa09f5f6e3e614672c45ddd0ad877b04f8e920
supreetkumar9496-del <supreetkumar9496@gmail.com>
2026-03-29 11:06:37 +0530

    .env config

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: './.env',
+})
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening at port ${process.env.PORT}`);
+})
